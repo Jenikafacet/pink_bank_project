1bc2ba70afa3cf3876515d0593344db8f3c5423b
ElVo <elvor.info@gmail.com>
2025-02-23 00:00:24 +0300

    add adaptive

--- assets/js/script.js
+++ assets/js/script.js
@@ -485,3 +485,11 @@ async function fetchWeather() {
     }
 }
 fetchWeather();
+
+/* гамбургер на меню */
+const burger = document.querySelector(".burger");
+    const menu = document.querySelector(".menu");
+
+    burger.addEventListener("click", function () {
+        menu.classList.toggle("active");
+    });
